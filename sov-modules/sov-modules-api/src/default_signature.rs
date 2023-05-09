9935c39f840f757677090d23034ff9dcec947cd0
bkolad <blazejkolad@gmail.com>
2023-05-09 21:28:12 +0200

    Import ed25519_dalek

--- sov-modules/sov-modules-api/src/default_signature.rs
+++ sov-modules/sov-modules-api/src/default_signature.rs
@@ -1,36 +1,60 @@
 use crate::{SigVerificationError, Signature};
 use borsh::{BorshDeserialize, BorshSerialize};
+use ed25519_dalek::ed25519::signature::Signature as DalekSignatureTrait;
+use ed25519_dalek::Verifier;
+use ed25519_dalek::{PublicKey as DalekPublicKey, Signature as DalekSignature};
 
-// TODO: https://github.com/Sovereign-Labs/sovereign/issues/253
-#[derive(PartialEq, Eq, Clone, BorshDeserialize, BorshSerialize, Debug)]
+#[derive(PartialEq, Eq, Clone, Debug)]
 pub struct DefaultPublicKey {
-    pub(crate) pub_key: Vec<u8>,
+    pub(crate) pub_key: DalekPublicKey,
+}
+
+impl BorshDeserialize for DefaultPublicKey {
+    fn deserialize_reader<R: std::io::Read>(reader: &mut R) -> std::io::Result<Self> {
+        todo!()
+    }
+}
+
+impl BorshSerialize for DefaultPublicKey {
+    fn serialize<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()> {
+        writer.write_all(self.pub_key.as_bytes())
+    }
 }
 
 impl DefaultPublicKey {
     pub fn new(pub_key: Vec<u8>) -> Self {
-        Self { pub_key }
+        //Self { pub_key }
+        todo!()
     }
 
-    pub fn sign(&self, _msg: [u8; 32]) -> DefaultSignature {
-        DefaultSignature {
-            msg_sig: vec![],
-            should_fail: false,
-        }
-    }
+    //pub fn sign(&self, _msg: [u8; 32]) -> DefaultSignature {
+    //    DefaultSignature { msg_sig: todo!() }
+    // }
 }
 
 impl<T: AsRef<str>> From<T> for DefaultPublicKey {
     fn from(key: T) -> Self {
         let key = key.as_ref().as_bytes().to_vec();
-        Self { pub_key: key }
+        //Self { pub_key: key }
+        todo!()
     }
 }
 
-#[derive(borsh::BorshDeserialize, borsh::BorshSerialize, PartialEq, Eq, Debug, Clone, Default)]
+#[derive(PartialEq, Eq, Debug, Clone)]
 pub struct DefaultSignature {
-    pub msg_sig: Vec<u8>,
-    pub should_fail: bool,
+    pub msg_sig: DalekSignature,
+}
+
+impl BorshDeserialize for DefaultSignature {
+    fn deserialize_reader<R: std::io::Read>(reader: &mut R) -> std::io::Result<Self> {
+        todo!()
+    }
+}
+
+impl BorshSerialize for DefaultSignature {
+    fn serialize<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()> {
+        writer.write_all(self.msg_sig.as_bytes())
+    }
 }
 
 impl Signature for DefaultSignature {
@@ -38,13 +62,12 @@ impl Signature for DefaultSignature {
 
     fn verify(
         &self,
-        _pub_key: &Self::PublicKey,
-        _msg_hash: [u8; 32],
+        pub_key: &Self::PublicKey,
+        msg_hash: [u8; 32],
     ) -> Result<(), SigVerificationError> {
-        if self.should_fail {
-            Err(SigVerificationError::BadSignature)
-        } else {
-            Ok(())
-        }
+        pub_key
+            .pub_key
+            .verify(&msg_hash, &self.msg_sig)
+            .map_err(|_| SigVerificationError::BadSignature)
     }
 }
